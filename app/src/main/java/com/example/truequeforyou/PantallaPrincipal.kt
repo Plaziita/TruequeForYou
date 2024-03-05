ba236d26a8200cceb2a335afbbb08f306acea46a
Plaziita <plazadiazmario@gmail.com>
2024-03-05 18:11:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/truequeforyou/PantallaPrincipal.kt
@@ -0,0 +1,25 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.truequeforyou
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.truequeforyou.bottomBar.Barra
+import com.example.truequeforyou.bottomBar.BotonesDeNavegar
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun PantallaMain(){
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { Barra(navController = navController)}
+    ) {
+        BotonesDeNavegar(navController = navController)
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/truequeforyou/bottomBar/BarraDeOpciones.kt
@@ -0,0 +1,42 @@
+package com.example.truequeforyou.bottomBar
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BarraDeOpciones(
+    val ruta : String,
+    val titulo: String,
+    val icono: ImageVector
+){
+    object Inicio : BarraDeOpciones(
+        ruta = "inicio",
+        titulo = "Inicio",
+        icono = Icons.Default.Home
+    )
+    object Favoritos : BarraDeOpciones(
+        ruta = "favoritos",
+        titulo = "Favoritos",
+        icono = Icons.Default.FavoriteBorder
+    )
+    object Tradear : BarraDeOpciones(
+        ruta = "tradear",
+        titulo = "Tradear",
+        icono = Icons.Default.ShoppingCart
+    )
+    object Mensajes : BarraDeOpciones(
+        ruta = "mensajes",
+        titulo = "Mensajes",
+        icono = Icons.Default.MailOutline
+    )
+    object Perfil : BarraDeOpciones(
+        ruta = "perfil",
+        titulo = "Perfil",
+        icono = Icons.Default.Person
+    )
+
+}
