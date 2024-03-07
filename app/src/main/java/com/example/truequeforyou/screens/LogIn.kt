0bcd1f12f18bfbe7ead6a208da94155f212757d6
Plaziita <plazadiazmario@gmail.com>
2024-03-07 20:06:31 +0100

    ventana login

--- app/src/main/java/com/example/truequeforyou/screens/LogIn.kt
+++ app/src/main/java/com/example/truequeforyou/screens/LogIn.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.truequeforyou.screens
 
 import androidx.annotation.StringRes
@@ -9,11 +11,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,16 +32,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.truequeforyou.R
 
 @Composable
 fun InicioSesion(){
-    var teclado by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -44,31 +52,38 @@ fun InicioSesion(){
                 .fillMaxSize()
                 .padding(16.dp)
         ) {
-            EditNumberField(
-                label = R.string.Correo,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Done
-                ),
-                value = teclado,
-                onValueChange = { teclado = it.toInt().toString() },
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text("Correo electrónico") },
                 modifier = Modifier
-                    .padding(bottom = 32.dp)
                     .fillMaxWidth()
-            )
-            EditNumberField(
-                label = R.string.Contraseña,
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    keyboardType = KeyboardType.Number,
-                    imeAction = ImeAction.Done
-                ),
-                value = teclado,
-                onValueChange = { teclado = it.toInt().toString() },
-                modifier = Modifier
-                    .padding(bottom = 32.dp)
-                    .fillMaxWidth()
+                    .padding(bottom = 16.dp),
+                leadingIcon = { Icon(Icons.Default.Email, contentDescription = null) },
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        // Handle the action when the "Done" button is pressed on the keyboard
+                    }
+                )
             )
 
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Contraseña") },
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = { Icon(Icons.Default.Lock, contentDescription = null) },
+                visualTransformation = PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        // Handle the action when the "Done" button is pressed on the keyboard
+                    }
+                )
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             Row(
                 modifier = Modifier
                     .fillMaxWidth(),
@@ -76,6 +91,9 @@ fun InicioSesion(){
             ){
                 Button(
                     onClick = {
+
+                        println("Correo electrónico: $email")
+                        println("Contraseña: $password")
 
                     }
                 ) {
