7f36e7740ea6fa558aac868cf85011f5ed072cc6
Luucasrsz <lucasrodriguezsanchez03@gmail.com>
2024-03-11 19:47:47 +0100

    lucascommit

--- app/src/main/java/com/example/intercromo/screens/RegistroEmail.kt
+++ app/src/main/java/com/example/intercromo/screens/RegistroEmail.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.intercromo.navigation.ventanasRegistro.VentanasLogIn
 import com.example.truequeforyou.navigation.ventanasRegistro.VentanasLogIn
 import com.google.firebase.auth.FirebaseAuth
 
